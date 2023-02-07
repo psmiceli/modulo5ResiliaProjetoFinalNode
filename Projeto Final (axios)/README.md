cffdd662033b98a61812956ede9025649a2b9202
Ronald <56053290+Ronald-Luiz@users.noreply.github.com>
2023-02-07 01:13:27 -0300

    Add files via upload

---
+++ Projeto Final (axios)/README.md
@@ -0,0 +1,60 @@
+# Projeto final do curso Programadores Cariocas.<br> 
+Módulo 5<br> 
+Plataforma:NodeJS<br>
+<h3>Tecnologias:</h3><br> 
+
+![image](https://user-images.githubusercontent.com/56053290/216841004-198317c1-cd02-4caa-9f08-40492a54bbde.png)
+![image](https://user-images.githubusercontent.com/56053290/216841015-6fce6cf2-8369-41af-b715-d3760249c5e9.png)
+![image](https://user-images.githubusercontent.com/56053290/216841119-36c482d3-5c2a-4bb8-b0c4-21b8a7b74531.png)
+
+<h1>Introdução </h1>
+
+<h4>Você e sua equipe foram escalados para desenvolverem
+propostas de APIs que serão o produto mínimo viável de um
+aplicativo.Vocês devem escolher o tema do aplicativo para
+identificar as entidades.</h4>
+
+Utilizar o padrão MVC;<br>
+⇨ Utilizar os verbos HTTP seguindo o padrão REST;<br>
+⇨ Implementar todas as operações de CRUD;<br>
+⇨ Utilizar o padrão de projeto (design pattern) DAO para abstração de transações no banco, com Promises;<br>
+⇨ Utilizar o README.md do repositório para documentação, contendo informações como:<br>
+◼ Como instalar as dependências do projeto;<br>
+◼ Como executar o projeto;<br>
+◼ Quais são as rotas possíveis;<br>
+◼ Quaisquer outros pontos que você achar necessários;<br>
+⇨ Utilização de async/await para operações no banco (DAO)<br>
+⇨ Ter o código fonte hospedado em um repositório no Github.<br>
+
+<h4>Nesse projeto você e sua squad serão responsáveis por
+definir quais são as entidades que o projeto precisa
+contemplar e cada um de vocês será responsável por
+implementar uma dessas entidades.</h4>
+
+<h2>🖥️Loja de informática</h2><br>
+
+![image](https://user-images.githubusercontent.com/56053290/216841605-304112c1-3aa1-481b-8544-ef54a783f10e.png)
+
+<h2>🗺️Mapeamento das Entidades</h2>
+<h3>Produtos:↴</h3>
+
+- <h4>Monitor</h4>
+- <h4>Placa de vídeo</h4>
+- <h4>dispositivo de armazenamento
+- <h4>Teclado</h4>
+- <h4>Mouse</h4>  
+
+<h2>🚀Frameworks utilizados no projeto:</h2><br>
+ ➤Axios<br>➤Json-server
+
+<h2>Como abrir o projeto❓</h2><br>
+<h3>⚙️Você precisará instalar os seguintes pacotes:</h3><br>
+
+➤ express: npm install express<br>
+➤ axios: npm install axios<br>
+➤ readline: npm install readline<br>
+➤ json-server: npm install -g json-server
+
+<h3>Em seguida execute o projeto:</h3>
+➤json-server --watch db.json<br>
+➤node app.js (em outro terminal)
---
+++ Projeto Final (axios)/app.js
@@ -0,0 +1,163 @@
+const express = require('express');
+const axios = require('axios');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const app = express();
+
+app.use(express.json())
+
+async function createRecord() {
+
+  const title = await new Promise(resolve => {
+    readline.question('Digite o produto para adicionar: ', resolve);
+  });
+
+  const body = await new Promise(resolve => {
+    readline.question('Digite as especificações: ', resolve);
+  });
+
+  const valor = await new Promise(resolve => {
+    readline.question('Digite o valor: ', resolve);
+  });
+
+
+  return axios.post('http://localhost:3000/produtos', {title, body, valor });
+}
+
+async function readRecord() {
+  const id = await new Promise(resolve => {
+    readline.question('Buscar produto: ', resolve);
+  });
+
+  return axios.get(`http://localhost:3000/produtos/${id}`);
+}
+
+async function updateRecord() {
+  const id = await new Promise(resolve => {
+    readline.question('Digite o ID do produto para atualizar: ', resolve);
+  });
+
+  const title = await new Promise(resolve => {
+    readline.question('Digite o novo título do produto: ', resolve);
+  });
+
+  const body = await new Promise(resolve => {
+    readline.question('Digite o novo conteúdo do produto: ', resolve);
+  });
+
+  const valor = await new Promise(resolve => {
+    readline.question('Digite o valor do produto: ', resolve);
+  });
+
+  return axios.put(`http://localhost:3000/produtos/${id}`, {title, body, valor });
+}
+
+async function deleteRecord() {
+  const id = await new Promise(resolve => {
+    readline.question('Digite o ID do produto que deseja excluir: ', resolve);
+  });
+
+  return axios.delete(`http://localhost:3000/produtos/${id}`);
+}
+
+async function showRecords() {
+  return axios.get(`http://localhost:3000/produtos`)
+    .then(response => {
+      console.log(response.data);
+    });
+}
+
+async function showOptions() {
+  console.log('Opções:');
+  console.log('1. Adicionar ao estoque');
+  console.log('2. Buscar produto');
+  console.log('3. Atualizar produto');
+  console.log('4. Excluir produto');
+  console.log('5. Estoque');
+  console.log('6. Sair');
+
+  const option = await new Promise(resolve => {
+    readline.question('Escolha uma opção: ', resolve);
+  });
+
+  switch (option) {
+    case '1':
+      createRecord()
+        .then(response => {
+          console.log(response.data);
+          return showOptions()
+        })
+        .catch(error => {
+          console.log(error);
+        });
+
+      break;
+
+    case '2':
+      readRecord()
+        .then(response => {
+          console.log(response.data);
+          return showOptions()
+        })
+        .catch(error => {
+          console.log(error);
+          return showOptions()
+        });
+      break;
+
+    case '3':
+      updateRecord()
+        .then(response => {
+          console.log(response.data);
+          return showOptions()
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      break;
+
+    case '4':
+      deleteRecord()
+        .then(response => {
+          console.log(response.data);
+          return showOptions()
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      break;
+
+
+    case '5':
+      showRecords()
+        .then(response => {
+          if (response && response.data) {
+            console.log(response.data);
+          } else {
+            console.error("");
+          }
+          return showOptions();
+        })
+        .catch(error => {
+          console.error(error);
+        });
+      break;
+
+      case '6':
+        readline.close();
+        break;
+      default:
+        console.log('Opção inválida, escolha novamente');
+        return showOptions();
+
+  }
+
+
+}
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado na porta 3000');
+  showOptions();
+});
