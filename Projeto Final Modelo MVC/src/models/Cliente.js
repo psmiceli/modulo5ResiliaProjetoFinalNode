98a718eff52d0046810c8fc195290faf1a352da7
Ronald <56053290+Ronald-Luiz@users.noreply.github.com>
2023-02-17 12:20:31 -0300

    Delete Projeto Final Modelo MVC/src directory

--- Projeto Final Modelo MVC/src/models/Cliente.js
+++
@@ -1,9 +0,0 @@
-class Cliente {
-    constructor(id, nome, CPF, endereco) {
-        this.id = id
-        this.nome = nome
-        this.CPF = CPF
-        this.endereco = endereco
-    }
-}
-module.exports = Cliente
--- Projeto Final Modelo MVC/src/models/Produto.js
+++
@@ -1,9 +0,0 @@
-class Produto {
-    constructor(id, modelo, especificacao, valor) {
-        this.id = id
-        this.modelo = modelo
-        this.especificacao = especificacao
-        this.valor = valor
-    }
-}
-module.exports = Produto
