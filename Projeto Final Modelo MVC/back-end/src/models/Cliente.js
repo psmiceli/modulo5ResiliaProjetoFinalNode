57774da5da38e8c8fc9145460ddb46c22ce841a1
Ronald <56053290+Ronald-Luiz@users.noreply.github.com>
2023-02-17 15:04:03 -0300

    Vou commitar um outro com correções

--- Projeto Final Modelo MVC/back-end/src/models/Cliente.js
+++
@@ -1,8 +0,0 @@
-class Cliente {
-    constructor(nome, CPF, endereco) {
-        this.nome = nome
-        this.CPF = CPF
-        this.endereco = endereco
-    }
-}
-module.exports = Cliente
--- Projeto Final Modelo MVC/back-end/src/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./index.js')
-
-// escolhendo a porta em que o servidor será aberto
-const port = 3000
-
-// abrindo o servidor na porta escolhida
-app.listen(port, () => {
-    console.log(`Server rodando em http://localhost:${port}/`)
-})
